bd7faae20ca6b36708f1673adb06eae7f3535b84
erikcc02 <designer231@gmail.com>
2022-01-03 09:32:52 -0300

    feat: implements extract headers to log

--- src/main.go
+++ src/main.go
@@ -1,19 +1,22 @@
 package main
 
 import (
+	"fmt"
 	"io"
 	"log"
 	"net/http"
 )
 
 const (
-	GrayColor = "\033[90m%s\033[0m"
+	GrayColor   = "\033[90m%s\033[0m"
+	YellowColor = "\033[1;33m%s\033[0m"
 )
 
 func main() {
 	echoHandler := func(w http.ResponseWriter, req *http.Request) {
 		log.Printf(GrayColor, "starting request")
 
+		extractHeaders(w, req)
 		io.WriteString(w, "teste")
 
 		log.Printf(GrayColor, "end request")
@@ -22,4 +25,11 @@ func main() {
 	http.HandleFunc("/", echoHandler)
 	log.Println("Listing for requests at http:localhost:8000")
 	log.Fatal(http.ListenAndServe(":8000", nil))
+}
+
+func extractHeaders(w http.ResponseWriter, req *http.Request) {
+	for k, v := range req.Header {
+		fmt.Fprintf(w, "%q: %q\n", k, v)
+		fmt.Printf(YellowColor, fmt.Sprintf("%s: %s\n", k, v))
+	}
 }
