c5e1db7d33519470f982850fbe56c8bbe5b838d6
erikcc02 <designer231@gmail.com>
2022-01-04 20:22:31 -0300

    feat: run app dynamic port

--- src/cmd/main.go
+++ src/cmd/main.go
@@ -1,14 +1,38 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"net/http"
+	"os"
+	"strconv"
 
 	"github.com/erikcc02/echo-server/src/internal/echo"
 )
 
+const ECHOSERVER_PORT string = "ECHOSERVER_PORT"
+
 func main() {
+
+	port := getAppPort(8003)
+
 	http.HandleFunc("/", echo.BuildHandler())
-	log.Println("Listing for requests at http:localhost:8000")
-	log.Fatal(http.ListenAndServe(":8000", nil))
+	log.Printf("Listing for requests at http:localhost:%d\n", port)
+	log.Fatal(http.ListenAndServe(fmt.Sprintf(":%d", port), nil))
+}
+
+func getAppPort(defaultPort int) int {
+
+	if len(os.Getenv(ECHOSERVER_PORT)) == 0 {
+		return defaultPort
+	}
+
+	port, err := strconv.Atoi(os.Getenv(ECHOSERVER_PORT))
+
+	if err != nil {
+		log.Printf("the application is going up on port %d...\n", defaultPort)
+		return defaultPort
+	}
+
+	return port
 }
